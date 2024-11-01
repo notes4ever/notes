c2cbbf24ad1e9870426f24141d97ab45867a4bc9
notes4ever <notes4ever@proton.me>
2024-11-01 23:19:04 +0800

    去掉twitter相关

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,6 @@ export const SITE = {
     'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
   homePage: 'https://notes4ever.vercel.app/',
   blogPage: 'https://notes4ever.vercel.app/',
-  twitterId: '',
   githubId: 'notes4ever',
   repo: 'notes4ever/notes',
 };
